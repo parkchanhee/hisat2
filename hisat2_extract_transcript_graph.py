fb9c73fb8dc68a64b5f5e889ae99bb15116c6390
Chanhee Park <parkchanhee@gmail.com>
2020-09-09 16:40:42 -0500

    WIP:

--- hisat2_extract_transcript_graph.py
+++ hisat2_extract_transcript_graph.py
@@ -28,79 +28,78 @@ import bisect
 bDebug = False
 bVerbose = False
 
-"""
-"""
-def read_genome(genome_file, chr_filter = None):
+
+def read_genome(genome_file, chr_filter=None):
     chr_dic = {}
-    
+    # chr_filter = [str(x) for x in list(range(1, 23)) + ['X', 'Y']]
+
+    def check_chr_filter(chr_name, chr_filter):
+        if chr_filter is None:
+            return True
+        if chr_name in chr_filter:
+            return True
+        return False
+
     chr_name, sequence = "", ""
     for line in genome_file:
         if line[0] == ">":
-            if chr_name and sequence:
+            if chr_name and sequence and check_chr_filter(chr_name, chr_filter):
                 chr_dic[chr_name] = sequence
             chr_name = line.strip().split()[0][1:]
             sequence = ""
         else:
             sequence += line[:-1]
 
-    if chr_name and sequence:
+    if chr_name and sequence and check_chr_filter(chr_name, chr_filter):
         chr_dic[chr_name] = sequence
 
-    #chr_filter = [str(x) for x in list(range(1, 23)) + ['X', 'Y']]
-    #chr_filter = None
+    if bVerbose:
+        lengths = [len(value) for value in chr_dic.values()]
+        print('Number of Chromosomes: {}'.format(len(chr_dic)))
+        print('Total length: {}'.format(sum(lengths)))
 
-    if chr_filter:
-        for chr_id, chr_seq in chr_dic.items():
-            if not chr_id in chr_filter: 
-                chr_dic.pop(chr_id, None)
-    
     return chr_dic
 
 
-"""
-"""
 def write_fasta(fp, ref_id, seq, width=60):
     print('>{}'.format(ref_id), file=fp)
 
     for i in range(0, len(seq), width):
-        print(seq[i:i+width], file=fp)
-    
+        print(seq[i:i + width], file=fp)
+
     return
 
 
-"""
-"""
 def load_transcript(genome_seq, gtf_file):
     genes = defaultdict(list)
     transcripts = {}
 
     # Parse valid exon lines from the GTF file into a dict by transcript_id
     for line in gtf_file:
-        if bVerbose:
-            line_org = str(line)
-
         line = line.strip()
         if not line or line.startswith('#'):
             continue
         if '#' in line:
             line = line.split('#')[0].strip()
+
         try:
             chrom, source, feature, left, right, score, \
                 strand, frame, values = line.split('\t')
         except ValueError:
             if bVerbose:
-                print("Warning: Can't parse line:", line_org)
-
+                print("Warning: Can't parse line:", line)
             continue
+
         if chrom not in genome_seq:
-            if bVerbse:
-                print('chr {} is not in genome'.format(chrom))
-
+            if bVerbose:
+                print('chr {} is not in genome: {}'.format(chrom, line))
             continue
-        
+
         # Zero-based offset
         left, right = int(left) - 1, int(right) - 1
-        if feature != 'exon' or left >= right:
+        if left >= right:
+            if bDebug:
+                print('Wrong position', line)
             continue
 
         values_dict = {}
@@ -108,71 +107,92 @@ def load_transcript(genome_seq, gtf_file):
             attr, _, val = attr.strip().partition(' ')
             values_dict[attr] = val.strip('"')
 
-        if 'gene_id' not in values_dict or \
-                'transcript_id' not in values_dict:
-            print('wrong line', line)
-            continue
+        if feature == 'gene':
+            gene_id = values_dict['gene_id']
+            if gene_id in genes:
+                print('Duplicated gene:', gene_id)
+                continue
 
-        transcript_id = values_dict['transcript_id']
-        gene_id = values_dict['gene_id']
+            gene_biotype = values_dict['gene_biotype']
+            gene_name = values_dict['gene_name']
+            genes[gene_id] = [list(), gene_biotype, gene_name]
 
-        if transcript_id not in transcripts:
-            transcripts[transcript_id] = [chrom, strand, [[left, right]], gene_id]
-            genes[values_dict['gene_id']].append(transcript_id)
+        elif feature == 'exon':
+            transcript_id = values_dict['transcript_id']
+            gene_id = values_dict['gene_id']
+
+            if transcript_id not in transcripts:
+                transcripts[transcript_id] = [chrom, strand, [[left, right]], gene_id]
+                genes[gene_id][0].append(transcript_id)
+            else:
+                transcripts[transcript_id][2].append([left, right])
         else:
-            transcripts[transcript_id][2].append([left, right])
+            continue
 
     return genes, transcripts
 
 
-
-"""
-"""
-def read_transcript(genome_seq, gtf_file, min_transcript_len = 0):
+def read_transcript(genome_seq, gtf_file, min_transcript_len=0):
     genes, transcripts = load_transcript(genome_seq, gtf_file)
 
+    merged_exon = 0
+    min_transcript_len_filtered = 0
+
     # Sort exons and merge where separating introns are <=5 bps
-    for tran, [chr, strand, exons, gene_id] in transcripts.items():
-            exons.sort()
-            tmp_exons = [exons[0]]
-            for i in range(1, len(exons)):
-                if exons[i][0] - tmp_exons[-1][1] <= 5:
-                    tmp_exons[-1][1] = exons[i][1]
-                else:
-                    tmp_exons.append(exons[i])
-            transcripts[tran] = [chr, strand, tmp_exons, gene_id]
+    for tran, [chrom, strand, exons, gene_id] in transcripts.items():
+        exons.sort()
+        tmp_exons = [exons[0]]
+        for i in range(1, len(exons)):
+            if exons[i][0] - tmp_exons[-1][1] <= 5:
+                merged_exon += 1
+                tmp_exons[-1][1] = exons[i][1]
+            else:
+                tmp_exons.append(exons[i])
+        transcripts[tran] = [chrom, strand, tmp_exons, gene_id]
 
     tmp_transcripts = {}
-    for tran, [chr, strand, exons, gene_id] in transcripts.items():
+    for tran, [chrom, strand, exons, gene_id] in transcripts.items():
         exon_lens = [e[1] - e[0] + 1 for e in exons]
         transcript_len = sum(exon_lens)
         if transcript_len >= min_transcript_len:
-            tmp_transcripts[tran] = [chr, strand, transcript_len, exons, gene_id]
+            tmp_transcripts[tran] = [chrom, strand, transcript_len, exons, gene_id]
+        else:
+            min_transcript_len_filtered += 1
 
     transcripts = tmp_transcripts
+
+    if bVerbose:
+        num_exons = [len(values[3]) for values in transcripts.values()]
+        total_exons = sum(num_exons)
+        avg_exons = total_exons / len(num_exons)
+
+        num_protein_coding = 0
+        for gene in genes.values():
+            if gene[1] == 'protein_coding':
+                num_protein_coding += 1
+
+        assert len(num_exons) == len(transcripts)
+        print("Number of genes: {}, protein_coding_genes: {}".format(len(genes), num_protein_coding))
+        print("Number of transcripts: {}".format(len(transcripts)))
+        print("merged_exon: {}, min_transcript_len_filtered: {}".format(merged_exon, min_transcript_len_filtered))
+        print("total exons: {}, average exons: {:.2f}".format(total_exons, avg_exons))
 
     return genes, transcripts
 
 
-
-
-"""
-"""
 def merge_range(A, B):
     assert A[0] <= B[0]
 
-    if (A[1]+1) < B[0]:
+    if (A[1] + 1) < B[0]:
         return None
 
     return [A[0], max(A[1], B[1])]
 
-"""
-"""
-def make_consensus_exons(exons_list):
 
+def make_consensus_exons(exons_list):
     assert len(exons_list) >= 1
 
-    exons_list = sorted(exons_list)
+    exons_list.sort()
 
     assert exons_list[0][0] <= exons_list[0][1]
     cons_exon_list = [exons_list[0]]
@@ -190,7 +210,6 @@ def make_consensus_exons(exons_list):
         else:
             cons_exon_list[-1] = m
 
-
     accum_len = 0
     for e in cons_exon_list:
         e.append(accum_len)
@@ -198,8 +217,7 @@ def make_consensus_exons(exons_list):
 
     return cons_exon_list
 
-"""
-"""
+
 def get_seq(ref, exons):
     seq = ''
 
@@ -209,14 +227,9 @@ def get_seq(ref, exons):
     return seq
 
 
-"""
-"""
 def write_transcripts_seq(fp, gene_id, chr_name, genome_seq, exon_list):
     chr_seq = genome_seq[chr_name]
-    seq = ''
-
-    for e in exon_list:
-        seq += chr_seq[e[0]:e[1] + 1]
+    seq = get_seq(chr_seq, exon_list)
 
     write_fasta(fp, gene_id, seq)
 
@@ -224,33 +237,30 @@ def write_transcripts_seq(fp, gene_id, chr_name, genome_seq, exon_list):
 
 
 def map_to_exons(old_exon, consensus_exon_list):
-
     for idx, cur_exon in enumerate(consensus_exon_list):
         if old_exon[0] >= cur_exon[0] and old_exon[1] <= cur_exon[1]:
             offset = old_exon[0] - cur_exon[0]
 
             return [cur_exon[2] + offset, cur_exon[2] + offset + (old_exon[1] - old_exon[0])], idx
 
-    print("Warning: Can't find old_exon", old_exon, file=sys.stderr)
+    print("Warning: Can't find old_exon", old_exon, file=stderr)
 
     return [-1, -1], -1
 
-"""
-"""
-def write_transcripts_snp(fp, gene_id, trans_ids, transcripts, exon_list):
 
+def write_transcripts_snp(fp, gene_id, trans_ids, transcripts, exon_list):
     snps = set()
 
     for trans_id in trans_ids:
         trans = transcripts[trans_id]
-       
+
         if bDebug:
             pprint.pprint(trans)
-        
+
         old_exons = trans[3]
 
         assert len(old_exons) >= 1
-       
+
         new_exons = list()
         for old_exon in old_exons:
             ne, idx = map_to_exons(old_exon, exon_list)
@@ -263,13 +273,10 @@ def write_transcripts_snp(fp, gene_id, trans_ids, transcripts, exon_list):
 
             new_exons.append(ne)
 
-
         for i in range(1, len(new_exons)):
-            gap = new_exons[i][0] - new_exons[i-1][1] - 1
+            gap = new_exons[i][0] - new_exons[i - 1][1] - 1
             if gap > 0:
-                snps.add((trans[0], new_exons[i-1][1] + 1, gap, trans_id))
-
-
+                snps.add((trans[0], new_exons[i - 1][1] + 1, gap, trans_id))
 
     snps_list = sorted(list(snps))
 
@@ -279,26 +286,22 @@ def write_transcripts_snp(fp, gene_id, trans_ids, transcripts, exon_list):
         print('{}.{}\t{}\t{}\t{}\t{}'.format(gene_id, snp_count, 'deletion', gene_id, s[1], s[2]), file=fp)
         snp_count += 1
 
-
     return
 
 
-"""
-"""
 def write_transcripts_ss(fp, gene_id, trans_ids, transcripts, exon_list):
-
     ss = set()
 
     for trans_id in trans_ids:
         trans = transcripts[trans_id]
-       
+
         if bDebug:
             pprint.pprint(trans)
-        
+
         old_exons = trans[3]
 
         assert len(old_exons) >= 1
-       
+
         new_exons = list()
         for old_exon in old_exons:
             ne, idx = map_to_exons(old_exon, exon_list)
@@ -311,25 +314,19 @@ def write_transcripts_ss(fp, gene_id, trans_ids, transcripts, exon_list):
 
             new_exons.append(ne)
 
-
         for i in range(1, len(new_exons)):
-            gap = new_exons[i][0] - new_exons[i-1][1] - 1
+            gap = new_exons[i][0] - new_exons[i - 1][1] - 1
             if gap > 0:
-                ss.add((new_exons[i-1][1], new_exons[i][0]))
-
-
+                ss.add((new_exons[i - 1][1], new_exons[i][0]))
 
     ss_list = sorted(list(ss))
 
     for s in ss_list:
         print('{}\t{}\t{}\t{}'.format(gene_id, s[0], s[1], '+'), file=fp)
 
-
     return
 
 
-"""
-"""
 def write_transcripts_map(fp, gene_id, chr_name, exon_list):
     print('>{}\t{}'.format(gene_id, chr_name), file=fp)
 
@@ -345,8 +342,7 @@ def write_transcripts_map(fp, gene_id, chr_name, exon_list):
 
     return
 
-"""
-"""
+
 def extract_transcript_graph(genome_file, gtf_file, base_fname):
     genome_seq = read_genome(genome_file)
     genes, transcripts = read_transcript(genome_seq, gtf_file)
@@ -362,24 +358,20 @@ def extract_transcript_graph(genome_file, gtf_file, base_fname):
     # get sorted id
     gene_ids = sorted(list(genes.keys()))
 
-    with open(base_fname + ".trans.fa", "w") as trseq_file,\
-        open(base_fname + ".trans.snp", "w") as trsnp_file,\
-        open(base_fname + ".trans.map", "w") as trmap_file,\
-        open(base_fname + ".trans.ss", "w") as trss_file:
+    with open(base_fname + ".trans.fa", "w") as trseq_file, \
+            open(base_fname + ".trans.snp", "w") as trsnp_file, \
+            open(base_fname + ".trans.map", "w") as trmap_file, \
+            open(base_fname + ".trans.ss", "w") as trss_file:
 
         # for each gene, build a consensus exons
         #   exon in consensus exons is not overlapped to other exon
-
         for gene_id in gene_ids:
-
             if bDebug:
                 if gene_id != "ENSG00000244625":
                     continue
 
-            trans_ids = genes[gene_id]
-
+            trans_ids = genes[gene_id][0]
             chrom = transcripts[trans_ids[0]][0]
-
             tmp_exons_list = list()
             for tid in trans_ids:
                 tmp_exons_list += transcripts[tid][3]
@@ -391,7 +383,7 @@ def extract_transcript_graph(genome_file, gtf_file, base_fname):
                 pprint.pprint(exon_list)
 
             # print sequeunce
-            write_transcripts_seq(trseq_file, gene_id, chrom, genome_seq, exon_list) 
+            write_transcripts_seq(trseq_file, gene_id, chrom, genome_seq, exon_list)
 
             # print snps
             write_transcripts_snp(trsnp_file, gene_id, trans_ids, transcripts, exon_list)
@@ -402,8 +394,6 @@ def extract_transcript_graph(genome_file, gtf_file, base_fname):
             # print exon_map
             write_transcripts_map(trmap_file, gene_id, chrom, exon_list)
 
-
-
             """
             exon_count = 0
             for t_id in genes[gene_id]:
@@ -411,17 +401,12 @@ def extract_transcript_graph(genome_file, gtf_file, base_fname):
             print(gene_id, len(genes[gene_id]), exon_count)
             """
 
-
     return
 
 
-"""
-for linear transcripts
-"""
 def write_exon_info(fp, tid, chr_name, strand, transcript_len, exons, gene_id):
     print('>{}\t{}\t{}\t{}\t{}'.format(tid, chr_name, strand, transcript_len, gene_id), file=fp)
 
-    buf = []
     for i, e in enumerate(exons):
         fp.write('{}:{}:{}'.format(chr_name, e[0], e[1] - e[0] + 1))
         if i % 4 == 3:
@@ -429,12 +414,8 @@ def write_exon_info(fp, tid, chr_name, strand, transcript_len, exons, gene_id):
         else:
             fp.write('\t')
 
-        #buf.append('{}:{}:{}\t'.format(chr_name, e[0], e[1] - e[0] + 1))
-
     if len(exons) % 4 != 0:
         fp.write('\n')
-
-    #print('\t'.join(buf), file=fp)
 
 
 def extract_transcript_linear(genome_file, gtf_file, base_fname):
@@ -443,64 +424,63 @@ def extract_transcript_linear(genome_file, gtf_file, base_fname):
 
     transcript_ids = sorted(list(transcripts.keys()))
 
-    with open(base_fname + ".trans.fa", "w") as trseq_file,\
-        open(base_fname + ".trans.map", "w") as trmap_file:
-
+    with open(base_fname + ".trans.fa", "w") as trseq_file, \
+            open(base_fname + ".trans.map", "w") as trmap_file:
         for tid in transcript_ids:
-            chr, strand, transcript_len, exons, gene_id = transcripts[tid]
-            
-            seq = get_seq(genome_seq[chr], exons)
-    
+            chrom, strand, transcript_len, exons, gene_id = transcripts[tid]
+
+            seq = get_seq(genome_seq[chrom], exons)
+
             write_fasta(trseq_file, tid, seq)
-            
+
             write_exon_info(trmap_file, tid, *transcripts[tid])
 
     return
 
+
 if __name__ == '__main__':
     parser = ArgumentParser(
-            description='Extract transcripts')
+        description='Extract transcripts')
 
     parser.add_argument('-g', '--gtf-file',
-            dest='gtf_file',
-            type=FileType('r'),
-            help='input GTF fie')
+                        dest='gtf_file',
+                        type=FileType('r'),
+                        help='input GTF fie')
 
     parser.add_argument('-r', '--genome',
-            dest='genome_file',
-            type=FileType('r'),
-            help='reference genome file')
+                        dest='genome_file',
+                        type=FileType('r'),
+                        help='reference genome file')
 
     parser.add_argument('-o', '--output',
-            dest='out_fname',
-            type=str,
-            help='output filename prefix')
+                        dest='out_fname',
+                        type=str,
+                        help='output filename prefix')
 
     parser.add_argument('--linear',
-            dest='bLinear',
-            action='store_true',
-            default=False,
-            help='Create linear-transcripts files')
+                        dest='bLinear',
+                        action='store_true',
+                        default=False,
+                        help='Create linear-transcripts files')
 
     parser.add_argument('--debug',
-            dest='bDebug',
-            action='store_true',
-            help='Run in debug mode')
+                        dest='bDebug',
+                        action='store_true',
+                        help='Run in debug mode')
     parser.add_argument('--verbose',
-            dest='bVerbose',
-            action='store_true',
-            help='Show more messages')
+                        dest='bVerbose',
+                        action='store_true',
+                        help='Show more messages')
 
     args = parser.parse_args()
     if not args.gtf_file or not args.genome_file or not args.out_fname:
         parser.print_help()
         exit(1)
 
-
-    if args.bDebug != None:
+    if args.bDebug is not None:
         bDebug = args.bDebug
 
-    if args.bVerbose != None:
+    if args.bVerbose is not None:
         bVerbose = args.bVerbose
 
     if args.bLinear:
@@ -513,4 +493,3 @@ if __name__ == '__main__':
             args.genome_file,
             args.gtf_file,
             args.out_fname)
-
